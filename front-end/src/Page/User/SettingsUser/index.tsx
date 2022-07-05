b0a613ec956b8901dd107cbcfba92a83b84afbc2
Erick <ericklima63@gmail.com>
2022-07-05 20:18:45 -0300

    Front: Settings Page Created

--- front-end/src/Page/User/SettingsUser/index.tsx
+++ front-end/src/Page/User/SettingsUser/index.tsx
@@ -1,5 +1,5 @@
 import * as C from './styles'
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
 import AsideNavLinks from '../../../Components/AsideNavLinks'
@@ -9,6 +9,13 @@ export default function SettingsUser() {
 
   const userLogin = useSelector((state: any) => state.userLogin)
   const { userInfo } = userLogin
+
+  const [tabIndex, setTabIndex] = useState<number>(0)
+
+  const nameRef = React.useRef() as React.MutableRefObject<HTMLInputElement>
+  const emailRef = React.useRef() as React.MutableRefObject<HTMLInputElement>
+  const passwordRef = React.useRef() as React.MutableRefObject<HTMLInputElement>
+  const confirmPassowrdRef = React.useRef() as React.MutableRefObject<HTMLInputElement>
 
   const navigate = useNavigate()
 
@@ -22,14 +29,79 @@ export default function SettingsUser() {
 
   }, [navigate, userInfo])
 
+  const handleSubmit = (e: React.FormEvent) => {
+
+    e.preventDefault()
+
+    //make submit
+
+  }
+
 
   return (
-    <C.Container>
+    <C.Container tabIndex={tabIndex}>
 
       <AsideNavLinks />
 
-      settings
+      <div className='content'>
 
+        <div className='menu-profile'>
+
+          <ul>
+            <li onClick={() => setTabIndex(0)}>User Profile</li>
+            <li onClick={() => setTabIndex(1)}>User ID</li>
+            <li onClick={() => setTabIndex(2)}>Bookmark Data</li>
+          </ul>
+
+        </div>
+
+        <div className='user' id='index-0'>
+
+          {userInfo && (
+            <h1>{userInfo.name}'s Profile</h1>
+          )}
+
+          <div className='user-avatar'>
+            <img src='https://i.pinimg.com/originals/8e/de/53/8ede538fcf75a0a1bd812810edb50cb7.jpg' alt='User Avatar' onClick={() => console.log('')}></img>
+            <div className='middle'>
+              <span>Change Avatar</span>
+            </div>
+          </div>
+
+          <form onSubmit={(e) => handleSubmit(e)}>
+
+            <div>
+              <label htmlFor='name'>Change Name</label>
+              <input type='text' id='name' placeholder={`${userInfo.name}`} ref={nameRef}></input>
+            </div>
+
+            <div>
+              <label htmlFor='email'>Change Email</label>
+              <input type='email' id='email' placeholder={`${userInfo.email}`} ref={emailRef}></input>
+            </div>
+
+            <div>
+              <label htmlFor='new-password'>Change Password</label>
+              <input type='password' id='new-password' placeholder='********' ref={passwordRef}></input>
+            </div>
+
+            <div>
+              <label htmlFor='confirm-new-password'>Confirm New Password</label>
+              <input type='password' id='confirm-new-password' placeholder='********' ref={confirmPassowrdRef}></input>
+            </div>
+
+            <div>
+              <button type='submit'>Submit Changes</button>
+            </div>
+
+          </form>
+
+        </div>
+
+        <div id='index-1'></div>
+        <div id='index-2'></div>
+
+      </div>
     </C.Container>
   )
 }
--- front-end/src/Page/User/SettingsUser/styles.ts
+++ front-end/src/Page/User/SettingsUser/styles.ts
@@ -1,8 +1,290 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+interface Props {
+
+    tabIndex: number
+
+}
+
+export const Container = styled.div<Props>`
+
+    display: flex;
+    flex-direction: row;
+
+    background-color: #fafafa;
+
+    div.content{
+
+        width: -webkit-fill-available;
+
+        margin: 0 2rem;
+        padding: 2rem 0;
+
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+
+        border-left: 2px solid #e6e6e6;
+
+        @media(max-width: 768px){
+
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+
+        }
+
+        @media(max-width: 620px){
+
+            border-left: 0;
+
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+
+        }
+    }
+
+    .menu-profile{
+        
+        width: 20%;
+
+        height: min-content;
+        background-color: transparent;
+
+        position: sticky;
+        top: 5vh;
+
+        @media(max-width: 768px){
+            
+            position: initial;
+
+            width: 80%;
+
+        }
+
+        @media(max-width: 620px){
+            
+            position: initial;
+
+            width: 100%;
+
+            margin-bottom: 2rem;
+
+        }
+
+        ul{
+            width: 100%;
+
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: flex-start;
+
+            background-color: #ffd0e3;
+            border-radius: 4px;
+
+            li{
+                cursor: pointer;
+
+                width: -webkit-fill-available;
+                height: 100%;
+
+                padding: 2rem;
+
+                font-size: 1.6rem;
+                font-weight: 600;
+                color: #444444;
+
+                border-bottom: 2px solid #ff1a75;
+
+                :hover{
+
+                    background-color: #ff1a75;
+                    color: #fff;
+
+                }
+
+                :first-child{
+
+                    :hover{
+
+                        border-radius: 4px 4px 0 0;
+
+                    }
+                }
 
 
+                :last-child{
+
+                    border-bottom: 0;
+
+                    :hover{
+                        
+                        border-radius: 0 0 4px 4px;
+                    }
+                    
+                }
+
+            }
+
+        }
+
+
+    }
+
+    div#index-0{
+        display: ${props => props.tabIndex === 0 ? `flex` : 'none'};
+    }
+    div#index-1{
+        display: ${props => props.tabIndex === 1 ? `flex` : 'none'};
+    }
+    div#index-2{
+        display: ${props => props.tabIndex === 2 ? `flex` : 'none'};
+    }
+
+    .user{
+            
+        width: 60%;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        @media(max-width: 768px){
+
+            width: 100%;
+
+        }
+
+        h1{
+            margin: 1rem 0;
+
+            font-size: 3rem;
+            font-weight: 400;
+        }
+
+
+        .user-avatar{
+
+            margin: 1rem 0;
+
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+
+            :hover{
+                    
+                img{
+                    
+                    opacity: 0.3;
+
+                }
+
+                div.middle{
+
+                    opacity: 1;
+
+                }
+
+            }
+
+            img{
+
+                cursor: pointer;
+
+                width: 20rem;
+                height: auto;
+                border-radius: 50%;
+                backface-visibility: hidden;
+                opacity: 1;
+
+                transition: all ease-in 0.1s;
+            }
+
+            >div{
+                
+                cursor: pointer;
+
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                opacity: 0;
+
+                width: 100%;
+
+                span{
+
+                    font-size: 1.8rem;
+                    color: #ff1a75;
+
+                }
+
+            }
+
+        }
+
+        form{
+
+            width: 100%;
+
+            >div{
+                
+                width: auto;
+                margin: 1rem;
+
+                display: flex;
+                flex-direction: column;
+
+                label{
+                    font-size: 1.8rem;
+                    font-weight: 400;
+                }
+                input{
+
+                    padding:  1rem;
+
+                    border: 1px solid #c0c0c0;
+                    border-radius: 4px;
+                    outline: 0;
+
+                    font-size: 1.8rem;
+                }
+                button{
+                    cursor: pointer;
+
+                    margin: 2rem 0;
+
+                    padding: 1.2rem 1rem;
+
+                    background-color: rgb(255, 82, 151);
+                    border: 0;
+                    border-radius: 4px;
+
+                    font-size: 1.8rem;
+                    color: #fff;
+
+                    transition: all ease-in 0.2s;
+
+                    :hover{
+                        box-shadow: 0px 0px 10px 0px #777777;
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
 
 
 `
