5c6a03d80b4113cc2f097d791432006677eafbc3
Erick Lima <ericklima63@gmail.com>
2024-02-03 14:10:07 -0300

    Fix Section Nav Though Medias Release on Home Page

--- front-end/app/lib/format_date_unix.ts
+++ front-end/app/lib/format_date_unix.ts
@@ -1,3 +1,4 @@
+// receive a number of days and returns the unix timestamp 
 export function convertToUnix(days: number) {
 
     if (days > 1) {
@@ -6,20 +7,26 @@ export function convertToUnix(days: number) {
         return timestamp
     }
 
+    // if days is 1 is meant to be as today
     const timestamp = Date.parse(new Date(Date.now()) as any) / 1000;
 
     return timestamp
 }
 
-export function convertFromUnix(days: number) {
+// get last minutes and seconds of this day (today) and returns the unix timestamp
+export function lastHourOfTheDay(days: number){
 
-    // if (days > 1) {
-    //     const timestamp = Date.parse(new Date(Date.now() - days * 24 * 60 * 60 * 1000) as any) / 1000;
+    const date = new Date(
+        Date.UTC(
+            new Date().getFullYear(),
+            new Date().getMonth(),
+            days == 1 ? new Date().getDate() : new Date().getDate() - days,
+            26,
+            59,
+            59
+        )
+    );
 
-    //     return timestamp
-    // }
+    return Math.floor(date.getTime() / 1000)
 
-    // const timestamp = Date.parse(new Date(Date.now()) as any) / 1000;
-
-    // return timestamp
 }
