b95320eea563e731acc8be3faf3e09841462bd7e
Erick Lima <ericklima63@gmail.com>
2024-02-11 21:18:25 -0300

    Add Media Format Icon over ArtCover

--- front-end/app/components/MediaItemCoverInfo/index.tsx
+++ front-end/app/components/MediaItemCoverInfo/index.tsx
@@ -3,9 +3,12 @@ import styles from './component.module.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import { ApiDefaultResult } from '@/app/ts/interfaces/apiAnilistDataInterface'
+import MovieSvg from "@/public/assets/film.svg"
+import AnimeSvg from "@/public/assets/play-circle.svg"
+import MangaSvg from "@/public/assets/book.svg"
+import MusicSvg from "@/public/assets/music-note-beamed.svg"
 
 function MediaItemCoverInfo({ positionIndex, data, darkMode, loading }: { data: ApiDefaultResult, positionIndex?: number, darkMode?: boolean, loading?: boolean }) {
-
     return (
 
         <div
@@ -20,9 +23,18 @@ function MediaItemCoverInfo({ positionIndex, data, darkMode, loading }: { data: 
                     alt={`Cover Art for ${data.title && data.title.romaji || "dsa"}`}
                     fill
                     sizes='100%'
+                    title={data.title.romaji || data.title.native}
                 ></Image>
-            </Link>
+                <span className={styles.media_type_icon}>
 
+                    {(data.format == "OVA" || data.format == "TV" || data.format == "ONA" || data.format == "SPECIAL") && <AnimeSvg width={16} height={16} alt="Tv Icon" />}
+                    {(data.format == "MOVIE") && <MovieSvg width={16} height={16} alt="Movie Icon" />}
+                    {(data.format == "MANGA") && <MangaSvg width={16} height={16} alt="Manga Icon" />}
+                    {(data.format == "MUSIC") && <MusicSvg width={16} height={16} alt="Music Icon" />}
+
+                </span>
+            </Link>
+            <p>{data.format}</p>
             <small>{(data.seasonYear && (`${data.seasonYear}, `))} {data.genres && data.genres[0]}</small>
 
             <Link href={`/media/${data.id}`}>{data.title && data.title.romaji}</Link>
