8411697c7b5ae48875451198486a8e3aa35f4985
Erick Lima <ericklima63@gmail.com>
2024-05-06 15:45:08 -0300

    refactor(style): Changes on Not Found and Error Page
    New Css file created for both Not Found and Error Page.

--- app/error.tsx
+++ app/error.tsx
@@ -1,87 +1,9 @@
 "use client"
-import React, { DetailedHTMLProps, HTMLAttributes, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import ErrorImg from "@/public/error-img-4.png"
 import Image from 'next/image'
 import { motion } from 'framer-motion'
-
-const errorContainerStyles = {
-    display: "flex",
-    overflow: "auto",
-    flexDirection: "column",
-    gap: "16px 0",
-    margin: "20px auto",
-    height: "80vh",
-    maxHeight: "700px",
-    background: 'var(--white-100)',
-    width: "96%",
-    maxWidth: "720px",
-    borderRadius: "8px",
-    boxShadow: "5px 5px 1px 0px var(--black-25)"
-
-}
-
-// img container
-const imgStylesContainer = {
-
-    display: "flex",
-    alignItems: "center",
-    margin: "auto",
-
-} as DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-
-// h1
-const errorH1Styles = {
-    fontWeight: "700",
-    color: "var(--error)",
-    padding: "16px"
-}
-
-// p
-const errorPStyles = {
-    fontWeight: "500",
-    padding: "16px"
-}
-
-const errorHeadingContainerStyles = {
-
-    display: "flex",
-    flexDirection: "column",
-    gap: "16px 0",
-    margin: "0 auto auto auto",
-    textAlign: "center",
-
-}
-
-const errorButtonContainerStyles = {
-
-    display: "flex",
-    gap: "0 16px",
-    margin: "32px auto",
-    maxWidth: "720px",
-
-}
-
-const errorRetryBtnContainerStyles = {
-
-    padding: "16px 8px",
-    border: "1px solid var(--brand-color)",
-    borderRadius: "4px",
-    background: "var(--white-100)",
-    color: "var(--brand-color) ",
-    fontWeight: "300"
-
-}
-
-const errorReturnHomeContainerStyles = {
-
-    padding: "16px 8px",
-    border: "1px solid var(--brand-color)",
-    borderRadius: "4px",
-    background: "var(--brand-color)",
-    color: "var(--white-100)",
-    fontWeight: "300"
-
-}
+import styles from "./errorStyles.module.css"
 
 function ErrorPage({ error, reset }: {
     error: Error & { digest?: string }
@@ -94,29 +16,28 @@ function ErrorPage({ error, reset }: {
 
     return (
 
-        <div style={errorContainerStyles as any}>
+        <div id={styles.error_container}>
 
-            <div style={imgStylesContainer}>
+            <div id={styles.img_container}>
                 <Image src={ErrorImg} height={240} alt={'Error 404'} />
             </div>
 
-            <div style={errorHeadingContainerStyles as any}>
-                <h1 style={errorH1Styles}>Something went wrong!</h1>
+            <div id={styles.heading_container}>
 
-                <p style={errorPStyles}>Sometimes is due to the API Hosting!</p>
+                <h1>Something went wrong!</h1>
 
-                <p style={errorPStyles}><b>Reloading Page Might Work!</b></p>
+                <p>Sometimes is due to the API Hosting! <b>Reloading Page Might Work!</b></p>
 
-                <span style={{ background: "var(--black-05)", padding: "8px", margin: "0 16px" }}>
-                    {error.name}: {error.message}
-                    <br />
-                    {error.stack}
+                <span>
+                    <b>{error.name}</b>: {error.message}
                 </span>
+
             </div>
 
-            <div style={errorButtonContainerStyles}>
+            <div id={styles.buttons_container}>
+
                 <motion.button
-                    onClick={() => reset()} style={errorRetryBtnContainerStyles as any}
+                    onClick={() => reset()}
                     whileTap={{ scale: 0.9 }}
                 >
                     Try again
@@ -124,7 +45,6 @@ function ErrorPage({ error, reset }: {
 
                 <motion.a
                     href={"/"}
-                    style={errorReturnHomeContainerStyles}
                     whileTap={{ scale: 0.9 }}
                 >
                     Return to Home Page
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,93 +1,31 @@
-import React, { DetailedHTMLProps, HTMLAttributes } from 'react'
+import React from 'react'
 import ErrorImg from "@/public/error-img-1.png"
 import Image from 'next/image'
+import styles from "./notFoundStyles.module.css"
 
 function Custom404() {
 
-    const errorContainerStyles = {
-        display: "flex",
-        flexDirection: "column",
-        gap: "16px 0",
-        margin: "20px auto",
-        minHeight: "80vh",
-        maxHeight: "700px",
-        background: 'var(--white-100)',
-        width: "96%",
-        maxWidth: "720px",
-        borderRadius: "8px",
-        boxShadow: "5px 5px 1px 0px var(--black-25)"
-
-    } as DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-
-    // img container
-    const imgStylesContainer = {
-
-        display: "flex",
-        alignItems: "center",
-        margin: "auto",
-
-    } as DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-
-    // h1
-    const errorH1Styles = {
-        fontWeight: "700",
-        color: "#f44336",
-        padding: "16px"
-    } as DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-
-    const errorHeadingContainerStyles = {
-
-        display: "flex",
-        flexDirection: "column",
-        gap: "32px 0",
-        margin: "auto",
-        textAlign: "center",
-        alignItems: "center"
-
-    } as DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-
-    const errorButtonContainerStyles = {
-
-        display: "flex",
-        gap: "0 16px",
-        margin: "32px auto",
-        maxWidth: "720px",
-
-    } as DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-
-    const errorReturnHomeContainerStyles = {
-
-        padding: "16px 8px",
-        border: "1px solid var(--brand-color)",
-        borderRadius: "4px",
-        background: "var(--brand-color)",
-        color: "var(--white-100)",
-        fontWeight: "300"
-
-    }
-
     return (
 
-        <div style={errorContainerStyles}>
+        <div id={styles.error_container}>
 
-            <div style={errorHeadingContainerStyles}>
+            <div id={styles.heading_container}>
 
-                <div style={imgStylesContainer}>
+                <div id={styles.img_container}>
                     <Image src={ErrorImg} height={330} alt={'Error 404'} />
                 </div>
 
-                <h1 style={errorH1Styles}>Page Not Found!</h1>
+                <h1>Page Not Found!</h1>
 
-                <small style={{ fontSize: "var(--font-size-p)" }}>
-                    {`We can't find the page you're trying to reach`}
-                </small>
+                <p>
+                    {`We couldn't find the page you're trying to reach.`}
+                </p>
+
             </div>
 
-            <div style={errorButtonContainerStyles}>
+            <div id={styles.button_container}>
 
-                <a href={"/"} style={errorReturnHomeContainerStyles}>
-                    Return to Home Page
-                </a>
+                <a href={"/"}>Return to Home Page</a>
 
             </div>
 
