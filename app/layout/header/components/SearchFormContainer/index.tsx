5928f1b65c036b64146b33787d692db4c249e223
Erick Lima <ericklima63@gmail.com>
2024-09-25 19:04:35 -0300

    style: Adult Content Alert on Search Results and bug fixes

--- app/layout/header/components/SearchFormContainer/index.tsx
+++ app/layout/header/components/SearchFormContainer/index.tsx
@@ -24,12 +24,12 @@ const framerMotionshowUpMotion = {
     y: "0",
     opacity: 1,
     transition: {
-      duration: 0.5,
+      duration: 0.2,
     },
   },
   exit: {
     opacity: 0,
-    y: "-120px",
+    y: "0",
   },
 };
 
