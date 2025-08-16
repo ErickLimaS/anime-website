bba9da592240bd75f2e50d24490ad9ebe25d9c05
Erick Lima <ericklima63@gmail.com>
2025-08-16 20:19:28 -0300

    refact: Fetch News From Backend

--- frontend/app/api/consumet/consumetNews.ts
+++ frontend/app/api/consumet/consumetNews.ts
@@ -1,53 +1,44 @@
 import { News } from "@/app/ts/interfaces/newsInterface";
 import Axios from "axios";
-import axiosRetry from "axios-retry";
-import { cache } from "react";
 
-const CONSUMET_API_URL = process.env.NEXT_PUBLIC_CONSUMET_API_URL;
-
-// HANDLES SERVER ERRORS, most of time when server was not running due to be using the Free Tier
-axiosRetry(Axios, {
-  retries: 3,
-  retryDelay: (retryAttempt) => retryAttempt * 2000,
-  retryCondition: (error) =>
-    error.response?.status == 500 || error.response?.status == 503,
-  onRetry: (retryNumber) =>
-    console.log(
-      `retry: ${retryNumber} ${retryNumber == 3 ? " - Last Attempt" : ""}`
-    ),
-});
+const NEXT_PUBLIC_NEXT_BACKEND_URL = process.env.NEXT_PUBLIC_NEXT_BACKEND_URL;
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   // GET ALL NEWS, OR NEWS BY TOPIC
-  getNews: cache(async ({ topic }: { topic?: string }) => {
+  getNews: async ({ topic }: { topic?: string }) => {
     try {
       const { data } = await Axios({
-        url: `${CONSUMET_API_URL}/news/ann/recent-feeds${topic ? `?topic=${topic}` : ""}`,
+        url: `${NEXT_PUBLIC_NEXT_BACKEND_URL}/news/consumet/ann/all`,
         method: "GET",
+        params: {
+          topic: topic,
+        },
       });
 
-      return data as News[];
+      console.log("News data:", data);
+
+      return data.result as News[];
     } catch (err) {
-      console.log(err);
+      console.error(err);
 
       return null;
     }
-  }),
+  },
 
   // GET NEWS ARTICLE BY ID
-  getNewsInfo: cache(async ({ id }: { id: string }) => {
+  getNewsInfo: async ({ id }: { id: string }) => {
     try {
       const { data } = await Axios({
-        url: `${CONSUMET_API_URL}/news/ann/info?id=${id}`,
+        url: `${NEXT_PUBLIC_NEXT_BACKEND_URL}/news/consumet/ann?id=${id}`,
         method: "GET",
       });
 
-      return data as News;
+      return data.result as News;
     } catch (err) {
-      console.log(err);
+      console.error(err);
 
       return err;
     }
-  }),
+  },
 };
