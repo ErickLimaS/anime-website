20431d19577df146182d1f885ad3d2201b791610
Erick Lima <ericklima63@gmail.com>
2025-08-16 19:39:59 -0300

    refact: Add Medias By Params Route routes with no format needed

--- backend/routes/getMediasByParamsRoute.js
+++ backend/routes/getMediasByParamsRoute.js
@@ -9,8 +9,11 @@ const mediasByFormatRoute = express.Router();
 // format available: TV_SHORT, MOVIE, SPECIAL, OVA, ONA, MUSIC, MANGA, NOVEL, ONE_SHOT
 // status available: "FINISHED" | "RELEASING" | "NOT_YET_RELEASED" | "CANCELLED" | "HIATUS"
 // Example: /medias/anime/TV?page=1&perPage=15&showAdultContent=true&sort=TRENDING_DESC&season=WINTER&seasonYear=2023&status=FINISHED
-mediasByFormatRoute.get('/anime/:format', mediasByParamsController.mediasByParamsOnAnilist); 
+mediasByFormatRoute.get('/anime', mediasByParamsController.mediasByParamsOnAnilist);
+mediasByFormatRoute.get('/anime/:format', mediasByParamsController.mediasByParamsOnAnilist);
+mediasByFormatRoute.get('/manga', mediasByParamsController.mediasByParamsOnAnilist);
 mediasByFormatRoute.get('/manga/:format', mediasByParamsController.mediasByParamsOnAnilist);
+mediasByFormatRoute.get('/movie', mediasByParamsController.mediasByParamsOnAnilist);
 mediasByFormatRoute.get('/movie/:format', mediasByParamsController.mediasByParamsOnAnilist);
 
 mediasByFormatRoute.get('/latest-releases', mediasByParamsController.mediasByParamsOnAnilist);
