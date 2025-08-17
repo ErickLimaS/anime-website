af5e8d9316015d3e15142fb7091ee0061e256edb
Erick Lima <ericklima63@gmail.com>
2025-08-17 19:24:49 -0300

    feat: Add Consumet Zoro on Episodes Route

--- backend/routes/mediaEpisodesRoute.js
+++ backend/routes/mediaEpisodesRoute.js
@@ -1,9 +1,13 @@
 const express = require("express")
-const episodesController = require("../controllers/episodes/aniwatch/episodesController");
+const aniwatchEpisodesController = require("../controllers/episodes/aniwatch/episodesController");
+const consumetZoroEpisodesController = require("../controllers/episodes/consumet/zoro/episodesController");
 
 const mediaEpisodesRoute = express.Router();
 
-mediaEpisodesRoute.get("/aniwatch/episode", episodesController.getEpisodeUrl())
-mediaEpisodesRoute.get("/aniwatch/all", episodesController.getEpisodesByMediaId())
+mediaEpisodesRoute.get("/aniwatch/episode", aniwatchEpisodesController.getEpisodeUrl())
+mediaEpisodesRoute.get("/aniwatch/all", aniwatchEpisodesController.getEpisodesByMediaId())
 
-module.exports = mediaEpisodesRoute    
+mediaEpisodesRoute.get("/consumet/zoro/episode", consumetZoroEpisodesController.getEpisodeUrl())
+mediaEpisodesRoute.get("/consumet/zoro/all", consumetZoroEpisodesController.getEpisodesByMediaId())
+
+module.exports = mediaEpisodesRoute     
