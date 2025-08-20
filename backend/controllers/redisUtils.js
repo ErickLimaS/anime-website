18f5388d2811edd7914e12bc6b7ccb116696f6c0
Erick Lima <ericklima63@gmail.com>
2025-08-20 16:19:42 -0300

    refactor: Update Redis expiration handling and improve error logging in setRedisKey function

--- backend/controllers/redisUtils.js
+++ backend/controllers/redisUtils.js
@@ -1,11 +1,11 @@
 async function setRedisKey({ redisClient, key, data }) {
-
-    await redisClient.set(key, JSON.stringify(data), {
-        type: "EX",
-        value: process.env.REDIS_EXPIRATION
-    })
-
-    return
+    try {
+        await redisClient.set(key, JSON.stringify(data));
+        await redisClient.expire(key, process.env.REDIS_EXPIRATION);
+        console.log(`#### -> Redis Key Set: Expiration ${process.env.REDIS_EXPIRATION} seconds`);
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 module.exports = {
