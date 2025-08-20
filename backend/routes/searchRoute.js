e6037a8251d312ac167ebd731b672b30b90fe14b
Erick Lima <ericklima63@gmail.com>
2025-08-20 13:25:57 -0300

    feat: Add Search from Anime Database Offline

--- backend/routes/searchRoute.js
+++ backend/routes/searchRoute.js
@@ -4,6 +4,7 @@ const searchOnGogoanimeController = require("../controllers/search/consumet/gogo
 const searchOnMangadexController = require("../controllers/search/consumet/mangadex/searchController");
 const searchOnAniwatchController = require("../controllers/search/aniwatch/searchController");
 const searchOnAnilistController = require("../controllers/search/anilist/searchController");
+const searchOnAnimeDatabaseController = require("../controllers/search/anime-database/searchController");
 
 const searchRoute = express.Router();
 
@@ -13,6 +14,7 @@ searchRoute.get("/anime/anilist", searchOnAnilistController.searchAnimeOnAnilist
 searchRoute.get(`/anime/aniwatch`, searchOnAniwatchController.searchAnimeOnAniwatch())
 searchRoute.get("/anime/consumet/zoro", searchOnZoroController.searchAnimeOnZoro())
 searchRoute.get(`/anime/consumet/gogoanime`, searchOnGogoanimeController.searchAnimeOnGogoanime())
+searchRoute.get(`/media/anime-database`, searchOnAnimeDatabaseController.searchMediasOnAnimeDatabase())
 
 // Search Manga 
 searchRoute.get(`/manga/consumet/mangadex/:query`, searchOnMangadexController.searchMangaOnMangadex())
