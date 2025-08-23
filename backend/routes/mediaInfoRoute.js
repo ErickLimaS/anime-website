3394105a08bde5dbd379fc8eb9cfe87490de5eed
Erick Lima <ericklima63@gmail.com>
2025-08-22 21:21:04 -0300

    feat: Implement Gogoanime media info retrieval and update routing

--- backend/routes/mediaInfoRoute.js
+++ backend/routes/mediaInfoRoute.js
@@ -3,10 +3,12 @@ const express = require("express")
 const mediaInfoRoute = express.Router();
 
 const mediaInfoOnZoroController = require("../controllers/mediaInfo/consumet/zoro/mediaInfoController");
+const mediaInfoOnGogoanimeController = require("../controllers/mediaInfo/consumet/gogoanime/mediaInfoController");
 const mediaInfoOnAnilistController = require("../controllers/mediaInfo/anilist/mediaInfoController");
 
 // Get Media Info => ?query=xxx
-mediaInfoRoute.get("/anime/consumet/zoro", mediaInfoOnZoroController.getMediaInfoOnZoro) 
+mediaInfoRoute.get("/anime/consumet/zoro", mediaInfoOnZoroController.getMediaInfoOnZoro)
+mediaInfoRoute.get("/anime/consumet/gogoanime", mediaInfoOnGogoanimeController.getMediaInfoOnGogoanime)
 mediaInfoRoute.get("/anime/anilist", mediaInfoOnAnilistController.geMediaInfoOnAnilist)
 
 module.exports = mediaInfoRoute
